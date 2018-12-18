2b2b649eaa891f62bfc4e96027579a2e5916290a
Sayaka Ono <sayakao@goopter.com>
2018-12-17 21:30:25 -0800

    create App class

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return <div>Hi there!</div>;
+}
+
+ReactDOM.render(
+  <SeasonDisplay />,
+  document.getElementById('root')
+);
+
+export default SeasonDisplay
